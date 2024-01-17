217b3260ab4c5b48fee2658bf9faa43a56005b0b
Zeem-K <zeemk74@gmail.com>
2024-01-17 15:58:47 +0100

    ca avance

--- Rope.py
+++ Rope.py
@@ -2,57 +2,69 @@ from collections import deque
 
 LEAF_LEN = 6
 class RopeNode:
-    def __init__(self,string):
-        self.string = string
+    def __init__(self,data):
+        self.data = data
         self.left = None
         self.right = None
-        self.length = len(string) if string else 0
-
-class Rope:
-    def __init__(self,string):
-        self.root = self.buildRopeNode(string)
-            
-    def buildRopeNode(self,string):
-        if not string:
-            return None
-        if len(string) > LEAF_LEN:
-            mid = len(string) // 2
-            node = RopeNode(None)
-            node.left = self.buildRopeNode(string[:mid])
-            node.right = self.buildRopeNode(string[mid:])
-            node.length  = self.calculate_length(node.left)
-            return node
-        return RopeNode(string)
-
-
-
-    def calculate_length(self, node):
+        self.length = len(data) if data else 0
+    
+    def height(self,node):
         if node is None:
             return 0
-        if node.left is None and node.right is None:
-            return node.length
-        
-        left_subtree_sum = self.calculate_length(node.left)
-        right_subtree_sum = self.calculate_length(node.right)
+        if node.left or node.right:
+            return max(self.height(node.left),self.height(node.right))
 
-        return left_subtree_sum + right_subtree_sum
+class Rope:
+    def __init__(self,data):
+        self.root = self.buildRopeNode(data)
+            
+    def buildRopeNode(self,data):
+        if not data:
+            return None
+        if len(data) > LEAF_LEN:
+            mid = len(data) // 2
+            node = RopeNode(None)
+            node.left = self.buildRopeNode(data[:mid])
+            node.right = self.buildRopeNode(data[mid:])
+            #Poids de chaque noeud vaut la somme des feuilles de son sous arbre gauche (D'après wikipédia)
+            # node.length  = self.calculate_length(node.left)
+            node.length = self.calculate_length(node.left)
+            node.length += self.calculate_length(node.right)
+            return node
+        return RopeNode(data)
+
+    def calculate_length(self,node):
+        return node.length if node else 0
+    
+
+    # Calacul du poids des neouds parent d'après wikipédia
+    # def calculate_length(self, node):
+    #     if node is None:
+    #         return 0
+    #     if node.left is None and node.right is None:
+    #         return node.length
         
+    #     left_subtree_sum = self.calculate_length(node.left)
+    #     right_subtree_sum = self.calculate_length(node.right)
+
+    #     return left_subtree_sum + right_subtree_sum
+        
+            
     def print_rope(self, node=None, indent=""):
         if not node:
             node = self.root
         if node:
-            print(f"{indent}[Length {node.length} , {node.string}]")
+            print(f"{indent}[Length {node.length} , {node.data}]")
             if node.left or node.right:
                 self.print_rope(node.left, indent + "  ")
                 self.print_rope(node.right, indent + "  ")
 
 
-
 def main():
-   rope_string = "hello_i_am_a_rope_data_structure"
-   print(len("hello_i_am_a_rope_data_structure"))
-   rope = Rope(rope_string)
+   rope_data = "happy_birthday!"
+   rope = Rope(rope_data)
    rope.print_rope()
+   
 
 if __name__ == "__main__":
     main()
