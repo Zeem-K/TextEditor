065bedfd4e2d85acbb84970c70d5b21694b3f66c
Lateef Oladimeji <oladimeji.lateef.etu@a13p20.fil.univ-lille.fr>
2024-01-17 11:21:23 +0100

    ffiahfi

--- Rope.py
+++ Rope.py
@@ -1,6 +1,8 @@
-LEAF_LEN = 8
+from collections import deque
+
+LEAF_LEN = 6
 class RopeNode:
-    def __init__(self,string) -> None:
+    def __init__(self,string):
         self.string = string
         self.left = None
         self.right = None
@@ -13,34 +15,42 @@ class Rope:
     def buildRopeNode(self,string):
         if not string:
             return None
-        if len(string) == 1:
-            return RopeNode(string)
+        if len(string) > LEAF_LEN:
+            mid = len(string) // 2
+            node = RopeNode(None)
+            node.left = self.buildRopeNode(string[:mid])
+            node.right = self.buildRopeNode(string[mid:])
+            node.length  = self.calculate_length(node.left)
+            return node
+        return RopeNode(string)
 
-        mid = len(string) // 2
-        node = RopeNode(None)
-        node.left = self.buildRopeNode(string[:mid])
-        node.right = self.buildRopeNode(string[mid:])
-        node.length = self.calculate_length(node.left) if node.left else 0
-        node.length += self.calculate_length(node.right) if node.right else 0
-        return node
-    
+
 
     def calculate_length(self, node):
-        return node.length if node else 0
+        if node is None:
+            return 0
+        if node.left is None and node.right is None:
+            return node.length
         
+        left_subtree_sum = self.calculate_length(node.left)
+        right_subtree_sum = self.calculate_length(node.right)
 
+        return left_subtree_sum + right_subtree_sum
+        
     def print_rope(self, node=None, indent=""):
         if not node:
             node = self.root
         if node:
-            print(indent + str(node.string))
+            print(f"{indent}[Length {node.length} , {node.string}]")
             if node.left or node.right:
                 self.print_rope(node.left, indent + "  ")
                 self.print_rope(node.right, indent + "  ")
 
 
+
 def main():
-   rope_string = "Je suis un hommme"
+   rope_string = "hello_i_am_a_rope_data_structure"
+   print(len("hello_i_am_a_rope_data_structure"))
    rope = Rope(rope_string)
    rope.print_rope()
 
